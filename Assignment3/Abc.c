345880579bc0b0ef6bd5cd4657c6bdd0459f2059
tanzim-rucse <tanzimislam1212@gmail.com>
2025-10-19 23:11:21 +0600

    Renamed variables for clarity

--- Assignment3/Abc.c
+++ Assignment3/Abc.c
@@ -1,44 +1,44 @@
 #include <stdio.h>
 
 int main() {
-    int a[3][3] = {
+    int marks[3][3] = {
         {50, 60, 70},
         {80, 90, 100},
         {30, 40, 50}
     };
     
-    char n[3][10] = {"Ali", "Bob", "Cat"};
-    int i, j, s;
-    float avg;
+    char students[3][10] = {"Ali", "Bob", "Cat"};
+    int i, j, total;
+    float average;
 
     for (i = 0; i < 3; i++) {
-        s = 0;
+        total = 0;
         for (j = 0; j < 3; j++) {
-            s += a[i][j];
+            total += marks[i][j];
         }
-        avg = s / 3.0;
-        if (avg >= 80)
-            printf("%s Grade A avg=%.2f\n", n[i], avg);
-        else if (avg >= 60)
-            printf("%s Grade B avg=%.2f\n", n[i], avg);
-        else if (avg >= 40)
-            printf("%s Grade C avg=%.2f\n", n[i], avg);
+        average = total / 3.0;
+        if (average >= 80)
+            printf("%s Grade A avg=%.2f\n", students[i], average);
+        else if (average >= 60)
+            printf("%s Grade B avg=%.2f\n", students[i], average);
+        else if (average >= 40)
+            printf("%s Grade C avg=%.2f\n", students[i], average);
         else
-            printf("%s Fail avg=%.2f\n", n[i], avg);
+            printf("%s Fail avg=%.2f\n", students[i], average);
     }
 
-    int max = 0, pos = 0;
+    int highestTotal = 0, topperIndex = 0;
     for (i = 0; i < 3; i++) {
-        s = 0;
+        total = 0;
         for (j = 0; j < 3; j++) {
-            s += a[i][j];
+            total += marks[i][j];
         }
-        if (s > max) {
-            max = s;
-            pos = i;
+        if (total > highestTotal) {
+            highestTotal = total;
+            topperIndex = i;
         }
     }
 
-    printf("Topper: %s with total %d\n", n[pos], max);
+    printf("Topper: %s with total %d\n", students[topperIndex], highestTotal);
     return 0;
 }
