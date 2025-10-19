ac37763fe1572509b103941bac2ea28113626778
tanzim-rucse <tanzimislam1212@gmail.com>
2025-10-19 23:16:40 +0600

    Added functions for average and grade calculation

--- Assignment3/Abc.c
+++ Assignment3/Abc.c
@@ -1,5 +1,24 @@
 #include <stdio.h>
 
+float calculateAverage(int marks[], int size) {
+    int total = 0;
+    for (int i = 0; i < size; i++) {
+        total += marks[i];
+    }
+    return total / 3.0;
+}
+
+void printGrade(char student[], float average) {
+    if (average >= 80)
+        printf("%s Grade A avg=%.2f\n", student, average);
+    else if (average >= 60)
+        printf("%s Grade B avg=%.2f\n", student, average);
+    else if (average >= 40)
+        printf("%s Grade C avg=%.2f\n", student, average);
+    else
+        printf("%s Fail avg=%.2f\n", student, average);
+}
+
 int main() {
     int marks[3][3] = {
         {50, 60, 70},
@@ -8,31 +27,12 @@ int main() {
     };
     
     char students[3][10] = {"Ali", "Bob", "Cat"};
-    int i, j, total;
-    float average;
-
-    for (i = 0; i < 3; i++) {
-        total = 0;
-        for (j = 0; j < 3; j++) {
-            total += marks[i][j];
-        }
-        average = total / 3.0;
-        if (average >= 80)
-            printf("%s Grade A avg=%.2f\n", students[i], average);
-        else if (average >= 60)
-            printf("%s Grade B avg=%.2f\n", students[i], average);
-        else if (average >= 40)
-            printf("%s Grade C avg=%.2f\n", students[i], average);
-        else
-            printf("%s Fail avg=%.2f\n", students[i], average);
-    }
-
     int highestTotal = 0, topperIndex = 0;
-    for (i = 0; i < 3; i++) {
-        total = 0;
-        for (j = 0; j < 3; j++) {
-            total += marks[i][j];
-        }
+
+    for (int i = 0; i < 3; i++) {
+        float average = calculateAverage(marks[i], 3);
+        printGrade(students[i], average);
+        int total = marks[i][0] + marks[i][1] + marks[i][2];
         if (total > highestTotal) {
             highestTotal = total;
             topperIndex = i;
